8dc350f57dff80b7a149dd4a0f1de1b7e3d2c830
kor5n <jegor.kotov@gmail.com>
2024-05-26 21:42:18 +0200

    added signing up and viewing your own tests

--- frontend/main.js
+++ frontend/main.js
@@ -1,12 +1,12 @@
 
-let cookies = document.cookie.split(";")
-let isCookieSaved = false
-let isLoggedIn = false
+//let cookies = document.cookie.split(";")
+//let isCookieSaved = false
+//let isLoggedIn = false
 
 let logInBtn = document.querySelector(".log-in")
 let signInBtn = document.querySelector(".sign-in")
 let profilePic = document.querySelector(".profile-pic")
-for (let i = 0; i< cookies.length; i++){
+/*for (let i = 0; i< cookies.length; i++){
     if(cookies[i].split("=")[0] == "isLoggedIn"){
         console.log(cookies[i].split("=")[0])
         isCookieSaved = true
@@ -14,24 +14,38 @@ for (let i = 0; i< cookies.length; i++){
         console.log(cookies[i].split("=")[1])
         break
     }
-}
+}*/
 
 async function getData(){
-    const response = await fetch("http://127.0.0.1:5000/main")
+    const response = await fetch("http://127.0.0.1:5000/tests")
     const data = await response.json()
-    console.log(data.tests)
-    data.tests.forEach(element => {
-        console.log(element["title"])
-        document.querySelector("main").innerHTML += `<div class="test-profile" onclick="location.href='view.html'">
-                                                <h3 class="test-name">${element["title"]}</h3>
-                                                <p class="quest-count">x questions</p>
-                                                <p class="author-name">author name</p>
-                                            </div>`
-    });
+    
+    if(data.message != 200 && data.message != 201){
+        alert(data.message)
+        /*if(data.message == "you are not logged in"){
+            window.location.assign("singup.html")
+        }*/
+    }
+    else{
+        logInBtn.style.display = "none"
+        signInBtn.style.display = "none"
+        profilePic.style.display = "inline"
+        console.log(data.tests)
+        data.tests.forEach(element => {
+            console.log(element["title"])
+            document.querySelector("main").innerHTML += `<div class="test-profile" onclick="location.href='view.html'">
+                                                    <h3 class="test-name">${element["title"]}</h3>
+                                                    <p class="quest-count">x questions</p>
+                                                    <p class="author-name">author name</p>
+                                                </div>`
+        })
+    }
+        
 
     
 }
-if (isCookieSaved){
+getData()
+/*if (isCookieSaved){
     if (isLoggedIn){
         logInBtn.style.display = "none"
         signInBtn.style.display = "none"
@@ -47,7 +61,7 @@ if (isCookieSaved){
     logInBtn.style.display = "inline-block"
     signInBtn.style.display = "inline-block"
     profilePic.style.display = "none"
-}
+}*/
 
 
 
--- frontend/signIn.js
+++ frontend/signIn.js
@@ -3,6 +3,11 @@ let logInDiv = document.querySelector(".log-in-div")
 let switchSignUp = document.querySelector('.sign-in')
 let switchLogIn = document.querySelector(".log-in")
 let eyeIcon = document.querySelector(".eye-icon")
+let logInBtn = document.querySelector(".login-btn")
+let signUpBtn = document.querySelector(".signup-btn")
+let emailSignUp = document.querySelector(".signup-email")
+let nameSignUp = document.querySelector(".name-input")
+let passwordSignUp = document.querySelector(".signup-password")
 
 switchSignUp.addEventListener("click", function(){
     signUpDiv.style.display = "flex"
@@ -15,7 +20,6 @@ switchLogIn.addEventListener("click", function(){
     eyeIcon.style.top = "34.25%"
 })
 eyeIcon.addEventListener("click", function(){
-    console.log("clicked")
     let passwordInput = document.querySelectorAll(".passwd-input")
     for (let i =0;i<passwordInput.length;i++){
         if(passwordInput[i].style.display != "none"){
@@ -25,6 +29,31 @@ eyeIcon.addEventListener("click", function(){
                 passwordInput[i].type = "text"
             }
         }
+    }
+})
+signUpBtn.addEventListener("click", async function(){
+    if (emailSignUp.value != "" && nameSignUp.value != "" && passwordSignUp.value != ""){
+        const data = {
+            "username":nameSignUp.value,
+            "email":emailSignUp.value,
+            "password":passwordSignUp.value
+        }
+        const options = {
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        }
+        const response = await fetch("http://127.0.0.1:5000/sign-up", options)
+        if (response.status != 201 && response.status != 200){
+            const resp = await response.json()
+            alert(resp.message)
+        }else{
+            window.location.assign("main.html")
+        }
+    }else{
+        alert("You must include a valid name, email and password")
     }
 })
 

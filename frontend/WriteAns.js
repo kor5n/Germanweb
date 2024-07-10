0e076ce779e57e8ba7cec90454e837be9ec3df0e
kor5n <jegor.kotov@gmail.com>
2024-07-10 20:32:18 +0200

    Added new styles as well as javascript file for write answer mode

---
+++ frontend/WriteAns.js
@@ -0,0 +1,83 @@
+let cookies = document.cookie.split(";")
+let session = ""
+let isCookieSaved = false
+let isViewing = false
+let realDefList = []
+
+for (let i = 0; i < cookies.length; i++) {
+    if (cookies[i].split("=")[0].replace(" ", "") == "user") {
+        isCookieSaved = true
+        session = cookies[i].split("=")[1]
+
+    }
+    if (cookies[i].split("=")[0].replace(" ", "") == "view_test") {
+        isViewing = true
+        viewing_test = cookies[i].split("=")[1]
+    }
+}
+
+function shuffle(array) {
+    let currentIndex = array.length;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+
+        // Pick a remaining element...
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+}
+
+const genTest = () => {
+    shuffle(defList)
+    console.log(defList)
+    console.log(realDefList)
+
+    for (let i = 0; i < defList.length; i++) {
+        document.querySelector("main").innerHTML += `<div class="answer-card">
+        <div class="question-half">
+            <h1 class="question-label">${defList[i]}</h1>
+        </div>
+        <div class="input-half">
+            <textarea cols="30" rows="1" class="answer-input" placeholder="Type answer"></textarea>
+            <button class="ans-btn">Submit answer</button>
+        </div>
+    </div>`
+        console.log(defList[i])
+        console.log(realDefList.indexOf(defList[i]))
+    }
+
+}
+
+const getTest = async () => {
+    const response = await fetch("http://127.0.0.1:5000/view/" + viewing_test)
+    const data = await response.json()
+    if (response.status != 200 && response.status != 201) {
+        window.alert(data.message)
+        document.cookie = `viewing_test=${viewing_test}; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`
+        window.location.assign("main.html")
+    } else {
+        termList = data.message[2].split(";")
+        defList = data.message[3].split(";")
+        realDefList = defList
+        console.log(realDefList)
+        document.querySelector(".flash-title").innerHTML = data.message[0]
+        genTest()
+    }
+}
+
+if (isCookieSaved) {
+    if (isViewing) {
+        getTest()
+    } else {
+        window.alert("No test was loaded")
+        window.location.assign("main.html")
+    }
+} else {
+    window.alert("You have to bet logged int to use this feature")
+    window.location.assign("signup.html")
+}

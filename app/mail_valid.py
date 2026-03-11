10ae29a5e15dde300a71ca244f986f5858cd9f2f
kor5n <jegor.kotov@gmail.com>
2026-03-11 11:28:01 +0100

    Chnaged prod email validation according to local

--- app/mail_valid.py
+++ app/mail_valid.py
@@ -1,21 +1,20 @@
 import requests
 from requests.structures import CaseInsensitiveDict
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
 def is_valid(email: str):
-    url = f"https://api.emailvalidation.io/v1/info?email={email}"
+    url = f"http://easyemailapi.com/api/verify/{email}"
 
-    headers = CaseInsensitiveDict()
-    headers["apikey"] = "nUH1hmV24lEwX1TIXmsgRPRRZw0L0NuOeHrdMp78"
-
+    headers = {"Authorization": f"Bearer {os.getenv("EMAIL_API_TOKEN")}"}
     response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        json_resp = response.json()
-        format_valid = json_resp["format_valid"]
-        mx_found = json_resp["mx_found"]
-        smtp_check = json_resp["smtp_check"]
-        state = json_resp["state"]
 
-        return format_valid and mx_found and smtp_check and state == "deliverable"
+    if response.status_code == 200:
+        data = response.json()
+        valid = data["valid"]
+
+        return valid
 
     return False
